c57eac8dd75a1299ed81905751c1638ac07dfaf5
cc164 <55838793+cc164@users.noreply.github.com>
2019-11-25 14:13:26 +0000

    Add files via upload

--- Public/Javascript/Client.js
+++ Public/Javascript/Client.js
@@ -6,17 +6,22 @@ var MyPlayerID;
 var PlayerState;
 var StartTime;
 var CurrentTime;
+var WaitTime;
+var TickTime;
 var BigTextDuration;
 var WinnerData;
+var ConnectionCheckers = [];
 
 var Canvas = document.getElementById("canvas");
 var ctx = Canvas.getContext("2d");
+var TextBox = document.getElementById("textInput");
+var Form = document.getElementById("form");
+var JoinButton = document.getElementById("join");
+var Heading = document.getElementById("heading");
 
 function Initialise() 
 {   
     GameInProgress = false;
-    
-    socket.emit('NewPlayer', {Name: "USERNAME"});
     
     //Initialise new game
     socket.on('NewGame', function(data)
@@ -25,6 +30,34 @@ function Initialise()
         MyPlayerID = data.MyPlayerID;
         PlayerState = 'alive';
         StartTime = new Date();
+        
+        if(MyPlayerID === -1)
+            PlayerState = 'spectator';
+        
+        ConnectionCheckers.push(setInterval(function()
+        {
+            if(new Date() - TickTime > 100)
+            {
+                if(PlayerState === 'spectator')
+                {
+                    ClearConnectionCheckers();
+                    
+                    GameInProgress = false;
+                    
+                    DrawObject([0, 0], [Canvas.width, Canvas.height], 'black');
+        
+                    JoinButton.style.display = 'block';
+                    Heading.style.display = 'block';
+                    Canvas.style.display = 'none';
+                }
+                else
+                {
+                    ShowError('ERROR: Connection dropped');
+                        
+                    ClearConnectionCheckers();
+                }
+            }
+        }, 50));
     });
     
     socket.on('UpdateMovement', function(data)
@@ -47,11 +80,99 @@ function Initialise()
         EndGame(data);
     });
     
+    socket.on('Full', function()
+    {
+        ShowError('Lobby is full');
+    });
+    
+    socket.on('SetStartTimer', function(data)
+    {
+        WaitTime = data.Time;
+        
+        var Text = 'Game Starting: ' + Math.floor(WaitTime / 1000);
+        
+        ctx.font = "45px Arial";
+        ctx.fillStyle = 'white';
+        ctx.fillText(Text, (Canvas.width / 2) - (ctx.measureText(Text).width / 2), 80);
+        
+        StartTimer = setInterval(function()
+        {
+            WaitTime-= 1000;
+            
+            if(!GameInProgress)
+            {
+                var Text = 'Game Starting: ' + Math.floor(WaitTime / 1000);
+
+                DrawObject([0, 0], [Canvas.width, Canvas.height], 'black');
+
+                ctx.font = "45px Arial";
+                ctx.fillStyle = 'white';
+                ctx.fillText(Text, (Canvas.width / 2) - (ctx.measureText(Text).width / 2), 80);
+            }
+            
+            if(WaitTime < 0)
+            {
+                TickTime = new Date();
+                
+                ConnectionCheckers.push(setInterval(function()
+                {
+                    if(new Date() - TickTime > 100)
+                    {
+                        ShowError('ERROR: Connection dropped');
+                        
+                        ClearConnectionCheckers();
+                    }
+                }, 50));
+                
+                WaitTime = 0;
+            
+                clearInterval(StartTimer);
+            }
+        }, 1000);
+    });
+    
+    socket.on('LeaderboardData', function(data)
+    {
+        if(data.LB === [])
+        {
+            var Table = '<p>ERROR: Cannot retrieve Leaderboard data</p>';
+        }
+        else    
+        {
+            var Table = '<table><tr><th>Username</th><th>Consecutive Rounds</th><th>Total Time</th></tr>'
+            
+            for(var i = 0; i < data.LB.length; i++)
+                Table+= '<tr><td>' + data.LB[i].Name + '</td><td>' + data.LB[i].Rounds + '</td><td>' + data.LB[i].Time / 1000 + 's</td></tr>';
+            
+            Table+= '</table>';
+        }
+
+        document.getElementById('table').innerHTML = Table;
+    });
+    
     //Adds listeners for when keys are pressed and released
     window.addEventListener("keydown", KeyDown);
     window.addEventListener("keyup", KeyUp);
 }
-    
+   
+function Join()
+{
+    if(TextBox.value !== '')
+    {
+        socket.emit('NewPlayer', {Name: TextBox.value});
+
+        Form.style.display = 'none';
+        JoinButton.style.display = 'none';
+        Heading.style.display = 'none';
+        Canvas.style.display = 'block';
+    }
+}
+
+function LoadLeaderboard()
+{
+    socket.emit('RequestLeaderboard');
+}
+
 function KeyDown(e)
 {
     if(getKeyID(e.keyCode) != 0 && GameInProgress)
@@ -117,8 +238,10 @@ function SendKeyPress(KeyID, Moving)
 //Draw a new frame with updated positions
 function UpdatePositions(PlayerPositions, ID, Stamina)
 {
-    //ctx.clearRect(0, 0, Canvas.width, Canvas.height);
+    TickTime = new Date();
+    
     DrawObject([0, 0], [Canvas.width, Canvas.height], 'black');
+    
     //Loop through all positions
     for(var i = 0; i < PlayerPositions.length; i++)
     {
@@ -127,7 +250,7 @@ function UpdatePositions(PlayerPositions, ID, Stamina)
             switch(PlayerPositions[i].ObjectTypeID)
             {
                 case 1:
-                    DrawObject(PlayerPositions[i].Position, [12, 12], 'blue');
+                    DrawObject(PlayerPositions[i].Position, [12, 12], 'blue', PlayerPositions[i].BorderColour);
                     break;
                     
                 case 2:
@@ -138,7 +261,7 @@ function UpdatePositions(PlayerPositions, ID, Stamina)
     
     //Draw this client's player if they are alive
     if(PlayerPositions[ID].Alive)
-        DrawObject(PlayerPositions[ID].Position, [12, 12], 'green');
+        DrawObject(PlayerPositions[ID].Position, [12, 12], 'green', PlayerPositions[ID].BorderColour);
     
     DrawUI(Stamina);
 }
@@ -177,9 +300,13 @@ function DrawUI(Stamina)
             if(WinnerData === undefined || WinnerData === null)
                 var Text = 'NOBODY WINS';
             else
-                var Text = WinnerData.Username + ' WINS: ' + (WinnerData.CompletionTime / 1000).toFixed(2) + 'S';
+                var Text = WinnerData.Username + ' WINS: ' + (WinnerData.CompletionTime / 1000).toFixed(2) + 's';
             
             ctx.fillText(Text, (Canvas.width / 2) - (ctx.measureText(Text).width / 2), BigTextDuration > 0 ? 60 : 30);
+            break;
+            
+        case 'spectator':
+            ctx.fillText("SPECTATOR", (Canvas.width / 2) - (ctx.measureText('SPECTATOR').width / 2), BigTextDuration > 0 ? 60 : 30);
             break;
             
         default:
@@ -187,9 +314,12 @@ function DrawUI(Stamina)
             break;
     }
     
-    ctx.font = "15px Arial";
-    ctx.fillStyle = 'white';
-    ctx.fillText((CurrentTime / 1000).toFixed(2) + 's', 10, 20);
+    if(PlayerState !== 'spectator' && CurrentTime !== undefined)
+    {
+        ctx.font = "15px Arial";
+        ctx.fillStyle = 'white';
+        ctx.fillText((CurrentTime / 1000).toFixed(2) + 's', 10, 20);
+    }
 }
 
 //Draws an object on the canvas
@@ -213,23 +343,53 @@ function DrawObject(Position, Size, Colour, BorderColour)
 
 function Eliminate(data)
 {
-    BigTextDuration = 512;
+    BigTextDuration = 128;
     
     PlayerState = 'eliminated';
 }
 
 function Safe()
 {
-    BigTextDuration = 512;
+    BigTextDuration = 128;
     
     PlayerState = 'safe';
 }
 
 function EndGame(Data)
 {
+    ClearConnectionCheckers();
+    
     BigTextDuration = 512;
     
     PlayerState = 'end';
     
     WinnerData = Data;
+    
+    GameInProgress = false;
+    
+    setTimeout(function()
+    {
+        DrawObject([0, 0], [Canvas.width, Canvas.height], 'black');
+        
+        JoinButton.style.display = 'block';
+        Heading.style.display = 'none';
+        Canvas.style.display = 'none';
+    }, 4000);
+}
+
+function ShowError(msg)
+{
+    DrawObject([0, 0], [Canvas.width, Canvas.height], 'black');
+    
+    ctx.font = "50px Arial";
+    ctx.fillStyle = 'white';
+    ctx.fillText(msg, (Canvas.width / 2) - (ctx.measureText(msg).width / 2), 60);
+}
+
+function ClearConnectionCheckers()
+{
+    for(var i = 0; i < ConnectionCheckers.length; i++)
+        clearInterval(ConnectionCheckers[i]);
+    
+    ConnectionCheckers = [];
 }
